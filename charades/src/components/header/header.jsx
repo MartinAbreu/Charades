4cfaf07e1018e47139c97c88bbc94ceee8bc627d
brianaabreu <briana.abreu01@gmail.comgit config --global user.email briana.abreu01@gmail.comgit config --global user.name brianaabreugit config --global user.email briana.abreu01@gmail.com>
2020-05-14 20:42:29 -0700

    added words/card/styling

--- charades/src/components/header/header.jsx
+++ charades/src/components/header/header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import logo from "../../images/CharadesLogo.png";
 
 import "./header.style.scss";
 
 const Header = () => (
   <div>
-    <h1>Charades</h1>
+    <img src={logo} alt="logo"></img>
   </div>
 );
 
---
+++ charades/src/objects.js
@@ -0,0 +1,89 @@
+const WORD_LIST = [
+    {
+        difficulty: "easy",
+        worth: 1,
+        words: [
+            "Hair Cut",
+            "Sewing",
+            "Playing Guitar",
+            "Painting your nails",
+            "Ride a bike",
+            "Putting on Makeup",
+            "Swimming",
+            "Playing Soccer",
+            "Juggle",
+            "Dreaming",
+            "Hopscotch",
+            "Backwards",
+            "Arm wrestle",
+            "Dancing",
+            "Robot",
+            "Smiling",
+            "Jumping Jacks",
+            "Stop",
+            "Pinch",
+            "Applause",
+            "Pray",
+            "Skipping",
+            "Elephant",
+            "Monkey",
+            "Brushing teeth",
+            "Airplane",
+            "Ringpop",
+            "Ping Pong",
+            "Playing Piano",
+            "Tickle",
+            
+
+        ]
+    },
+
+    {
+        difficulty: "medium",
+        worth: 2,
+        words: [
+            "Pedicure",
+            "Hitch Hike",
+            "Flip",
+            "Playing Baseball",
+            "Dentist",
+            "Ironing Clothes",
+            "Using a jackhammer",
+            "Sleep Walking",
+            "Riding a Skateboard",
+            "Kneel",
+            "Chomping food",
+            "Photographer",
+            "Surfing",
+            "Kangaroo",
+            "Karate",
+            "Hula Hooping",
+            "Whispering",
+            "Seesaw",
+            "Blind",
+            "Twist",
+
+        ]
+    },
+
+    {
+        difficulty: "hard",
+        worth: 3,
+        words: [
+            "Baking",
+            "Folding Laundry",
+            "Curling Hair",
+            "Using Chopsticks",
+            "Riding a Carousel",
+            "Skiing",
+            "Mowing a Lawn",
+            "Food Fight",
+            "Finger Painting",
+            "Watching a Scary Movie",
+            "Reading a Newspaper",
+            "Funnybone",
+            "Michael Jackson",
+
+        ]
+    }
+]
